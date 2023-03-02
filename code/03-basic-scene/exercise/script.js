044637d2c5bfa3807a981b4a63beac729c506272
Raul <9953156+raul23@users.noreply.github.com>
2023-03-02 15:20:54 -0500

    Update script.js

--- code/03-basic-scene/exercise/script.js
+++ code/03-basic-scene/exercise/script.js
@@ -1,1 +1,35 @@
+import * as THREE from 'three';
+console.log('Hello Three.js');
 
+// 4 elements to get started: scene, objects, camera, and renderer
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
+const cubeMaterial = new THREE.MeshBasicMaterial({
+    color: '#ff0000'
+})
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
+scene.add(cubeMesh)
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
